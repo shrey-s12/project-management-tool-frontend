e7ee1ce5fdf6e71ec32914d8c93cc44f253051dc
Shrey Singhal <shreynbd12@gmail.com>
2024-09-08 10:54:32 +0530

    fixed login page

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -37,15 +37,20 @@ const Sidebar = () => {
       {/* Dark Mode Toggle */}
       <div className="p-4 flex items-center justify-between">
         <span>{darkMode ? 'Dark Mode' : 'Light Mode'}</span>
-        <label className="relative inline-flex items-center cursor-pointer">
+        <div className="relative">
+          {/* Toggle Switch */}
           <input
             type="checkbox"
+            id="darkModeToggle"
             checked={darkMode}
             onChange={toggleDarkMode}
             className="sr-only peer"
           />
-          <div className="w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-        </label>
+          <div className="w-12 h-6 bg-gray-300 rounded-full peer-checked:bg-yellow-500 peer-focus:ring-4 peer-focus:ring-yellow-300 transition-colors duration-300 ease-in-out"></div>
+          <div
+            className="absolute left-1 top-1 w-4 h-4 bg-white rounded-full transition-transform duration-300 ease-in-out peer-checked:translate-x-6"
+          ></div>
+        </div>
       </div>
     </div>
   );
